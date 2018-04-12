7e16506a60b8fdcc4ce1dd600a40bac9dac0c15f
SpiveeWorks <spiveehere@gmail.com>
2018-04-12 22:26:23 +1000

    function parsing

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -6,7 +6,7 @@ mod tests {
 
     macro_rules! parses {
         ($parser: expr, $text: expr) => {{
-            let parser = $parser;
+            let parser = &$parser;
             let original = $text;
             let parsed = parser.parse(original).unwrap();
             let regenerated = format!("{}", parsed);
@@ -15,9 +15,17 @@ mod tests {
     }
 
     #[test]
-    fn parses() {
-        let expr_parser = readable::ExprParser::new();
-        parses!(expr_parser, "a b (c d)");
+    fn expr_parsing() {
+        let parser = readable::ExprParser::new();
+        parses!(parser, "a b (c d)");
+    }
+
+    #[test]
+    fn function_parsing() {
+        let parser = readable::FunParser::new();
+        parses!(parser, "id (x: T) = x");
+        parses!(parser, "const (x: T) (y: U) = x");
+        parses!(parser, "inl (x: T) = Pair true x");
     }
 }
 
